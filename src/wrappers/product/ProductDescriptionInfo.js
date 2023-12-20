79b1e12b3ea145b394dafe62a4522fc497b9f051
Heena-Kapoor <hkkapoor257@gmail.com>
2023-12-20 16:57:58 +0530

    Updated banner

--- src/wrappers/product/ProductDescriptionInfo.js
+++ src/wrappers/product/ProductDescriptionInfo.js
@@ -40,7 +40,7 @@ const ProductDescriptionInfo = ({
   return (
     <div className="product-details-content ml-70">
       <h2>{product.name}</h2>
-      {/* <div className="product-details-price">
+      <div className="product-details-price">
         {discountedPrice !== null ? (
           <Fragment>
             <span>{currency.currencySymbol + finalDiscountedPrice}</span>{" "}
@@ -51,7 +51,7 @@ const ProductDescriptionInfo = ({
         ) : (
           <span>{currency.currencySymbol + finalProductPrice} </span>
         )}
-      </div> */}
+      </div>
       {product.rating && product.rating > 0 ? (
         <div className="pro-details-rating-wrap">
           <div className="pro-details-rating">
